314b52483f5ee8058e81b01a225251616082e203
iamgauravkha <gauravk@oplify.in>
2026-03-21 00:53:48 +0530

    feat: Add mentee suggestion functionality and improve loading state handling

--- services/dashboardApi.js
+++ services/dashboardApi.js
@@ -1,6 +1,9 @@
 import axios from "@/utils/axios";
 
-export const getSuggetededStudentsApi = (params) => axios.get(`/company/suggested-students`, { params });
+export const getSuggetededStudentsApi = (params) =>
+  axios.get(`/company/suggested-students`, { params });
 
-export const getSuggestedMentorsApi = (params) => axios.get(`/student/suggested-mentors`, { params });
-
+export const getSuggestedMentorsApi = (params) =>
+  axios.get(`/student/suggested-mentors`, { params });
+export const getSuggestedMenteesApi = (params) =>
+  axios.get(`/mentor/suggested-mentee`, { params });
